0f28936d5e3f686aab0996b8a2792815a5161594
Rishant Dutt <rishant.dutt96@gmail.com>
2019-02-05 20:30:00 +0530

    Simple cloning added.

---
+++ Chapter 3 - Prototype Pattern/cloning.py
@@ -0,0 +1,23 @@
+import copy
+
+
+class A:
+    def __init__(self):
+        self.x = 18
+        self.msg = 'Hello'
+
+
+class B(A):
+    def __init__(self):
+        super().__init__()
+        self.y = 34
+
+    def __str__(self):
+        return f'{self.x}, {self.msg}, {self.y},'
+
+
+if __name__ == '__main__':
+    b = B()
+    c = copy.deepcopy(b)
+    print([str(itm) for itm in (b, c)])
+    print([i for i in (b, c)])
